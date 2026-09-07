1d1c49b3d173e6b58f8daf7bab7432f90a183da5
Siksaka Suriyasat <113951267+siksakas@users.noreply.github.com>
2026-09-07 12:33:19 -0400

    week1 hw

---
+++ wk1hw.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+
+func createIsland(islandSize: Int) {
+    for i in 1...islandSize {
+        var str = ""
+        for j in 1...5 {
+            let randomInt = Int.random(in: 0...10)
+            switch randomInt {
+            case 0: str.append("🐟")
+            case 1: str.append("🐠")
+            case 2: str.append("🏝️")
+            default: str.append("🌊")
+            }
+        }
+        print(str)
+    }
+    print()
+}
+
+createIsland(islandSize: 5)
+
+createIsland(islandSize: 10)
